6dafb0736eceb7a3bace1f43bde075bebf72e5f8
Aryaputra Athallah <aryaputra.athallah@ui.ac.id>
2020-10-17 16:54:05 +0700

    Edit minimal char

--- matakuliah/models.py
+++ matakuliah/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class MataKuliahSaya(models.Model):
-    nama = models.CharField(max_length = 50)
-    dosen = models.CharField(max_length = 50)
+    nama = models.CharField(max_length = 100)
+    dosen = models.CharField(max_length = 100)
     jumlahSKS = models.PositiveIntegerField()
-    semester = models.CharField(max_length = 20)
-    ruangKelas = models.CharField(max_length= 50)
+    semester = models.CharField(max_length = 100)
+    ruangKelas = models.CharField(max_length= 100)
     deskripsi = models.CharField(max_length = 500)
 
     def __str__(self):
